1bf6ba511f2aa060b183363e7f170a43f9a5746c
Thiago Bianeck <thiago.bianeck@viasoft.com.br>
2021-01-07 17:32:11 -0300

    Estrutura do server

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
 import "./App.scss";
+import { DatePicker } from "antd";
+
+const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+
+const onChange = (date, dateString) => {
+  console.log(date, dateString);
+};
 
 function App() {
   return (
     <div className="app">
       <h1>Web Personal Cliente</h1>
+      <div>
+        <DatePicker onChange={onChange} />
+        <br />
+        <MonthPicker onChange={onChange} placeholder="Select month" />
+        <br />
+        <RangePicker onChange={onChange} />
+        <br />
+        <WeekPicker onChange={onChange} placeholder="Select week" />
+      </div>
     </div>
   );
 }
